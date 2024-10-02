596d029b1890c673162214a126ada117779f476e
anish <anish.shrestha@ut.ee>
2024-10-02 09:57:39 +0300

    used ZED odometry to compute gps heading updates

--- Jackal/robot_orienteering/nodes/heading_publisher.py
+++ Jackal/robot_orienteering/nodes/heading_publisher.py
@@ -13,53 +13,33 @@ class HeadingPublisher:
 
         
         self.initial_heading = rospy.get_param('~initial_heading')
-        self.current_heading = self.limit_angle(self.initial_heading)
-
-        rospy.loginfo(f"Current gps heading: {self.current_heading}")
-        print("----------------------------------------------------")
-
-        self.initial_yaw = None
-        self.prev_yaw = None
+        self.initial_heading = self.limit_angle(self.initial_heading)
         
         self.current_heading_publisher = rospy.Publisher('/current_heading',
                                                          Float32,
                                                          queue_size=1)
         
-        rospy.Subscriber('/odometry/filtered', Odometry, self.odom_callback, queue_size=1)
+        rospy.Subscriber('/zed/zed_node/odom', Odometry, self.odom_callback, queue_size=1)
 
 
     def limit_angle(self, angle):
         return (angle + 180) % 360 - 180
 
     def odom_callback(self, msg):
+        # Convert quaternion to euler angles
+        quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(quaternion)
 
-        current_quaternion = np.array([msg.pose.pose.orientation.x, 
-                                       msg.pose.pose.orientation.y, 
-                                       msg.pose.pose.orientation.z, 
-                                       msg.pose.pose.orientation.w])  
-        
-        current_yaw = tf.transformations.euler_from_quaternion(current_quaternion)[2] 
-        current_yaw = np.rad2deg(current_yaw)
-        current_yaw = self.limit_angle(current_yaw)
+        # Extract yaw (z-axis rotation) and convert to degrees
+        yaw = euler[2]  # Yaw is the third element (index 2)
+        yaw_degrees = np.degrees(yaw)
 
-        if self.prev_yaw is None:
-            self.prev_yaw = current_yaw
-            self.prev_yaw = self.limit_angle(self.prev_yaw)
-        
-        delta_yaw = current_yaw - self.prev_yaw
-        
-        if abs(delta_yaw) >= 0.1:
-            self.current_heading -= delta_yaw
-
-        self.current_heading = self.limit_angle(self.current_heading)
+        gps_heading = self.initial_heading - yaw_degrees
+        gps_heading = self.limit_angle(gps_heading)
 
         current_heading = Float32()
-        current_heading.data = self.current_heading
+        current_heading.data = gps_heading
         self.current_heading_publisher.publish(current_heading)
-
-        rospy.loginfo(f"Current gps heading: {self.current_heading}")
-
-        self.prev_yaw = current_yaw
         
         
 if __name__ == "__main__":
