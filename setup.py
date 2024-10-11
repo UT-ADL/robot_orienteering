3a80bd4f5563d81044dce365b4303385bed697e5
anish <anish.shrestha@ut.ee>
2024-10-11 12:35:56 +0300

    added gps_parser node in catkin_python_setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    scripts=['nodes/deadman_switch.py',
-             'nodes/heading_publisher.py',
-             'nodes/robot_drive.py'],
+    scripts=['nodes/deadman_switch.py',             
+             'nodes/robot_drive.py',
+             'nodes/gps_parser.py'],
     packages=['global_planner', 'utils', 'waypoint_planner'],
     package_dir={'': 'src'},
     requires= ['rospy']
